f51b1bf171fc89a61e13faa5a042bd1bb46ee78a
Yann Hamdaoui <yann.hamdaoui@gmail.com>
2020-12-01 11:24:04 +0100

    Update scripts to generate and check Makefile

--- compare.sh
+++ compare.sh
@@ -2,10 +2,13 @@
 # Use the json_diff tool to compare the Nickel export and the original json.
 
 files=("guestbook-controller" "guestbook-service" "redis-master-service" "redis-master-controller" "redis-slave-controller" "redis-slave-service")
+makefile="Makefile.gen"
 
 for f in "${files[@]}"
 do
     echo "** Processing ${f}"
     json_diff f "${f}.json" "${f}.ncl.json"
 done
+
+diff Makefile ${makefile}
 echo "Done."
--- export.sh
+++ export.sh
@@ -3,6 +3,7 @@
 # files.
 
 files=("guestbook-controller" "guestbook-service" "redis-master-service" "redis-master-controller" "redis-slave-controller" "redis-slave-service")
+makefile="Makefile.gen"
 
 for f in "${files[@]}"
 do
@@ -10,4 +11,8 @@ do
     rm -f "${f}.ncl.json"
     nickel -f "${f}.ncl" export -o "${f}.ncl.json"
 done
+
+echo "** Generating Makefile"
+rm -f "${makefile}"
+nickel -f "Makefile.ncl" export -o "${makefile}" --format raw
 echo "Done."
